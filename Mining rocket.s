5b8d30d41d20cf7cae3cda1820d1dcdfaacb40d3
Jon Dunlap <jon@the364.com>
2021-07-08 21:34:18 -0400

    Fixed Mining rocket

--- Mining rocket.s
+++ Mining rocket.s
@@ -1,11 +1,11 @@
 alias button d0
 alias countled d1
 
-define AUTOMOD 0
-define OREMOD 0
-define SILOMOD 0
-define FUELMOD 0
-define KLAXON 0
+define AUTOMOD 1556756312
+define OREMOD -2027419821
+define SILOMOD 906210602
+define FUELMOD -505333199
+define KLAXON -828056979
 
 wait:
 yield
@@ -16,19 +16,19 @@ move r0 5
 move r1 33
 s button Lock 1
 sb KLAXON On 1
-sb KLAXON Setting 35
+sb KLAXON Mode 35
 sleep 5
 s button Open 0
 countdown:
 s countled Setting r0
-sb KLAXON Setting r1
+sb KLAXON Mode r1
 sleep 1
 sub r0 r0 1
 sub r1 r1 1
 bgtz r0 countdown
 
 s countled Setting 0
-sb KLAXON Setting 36
+sb KLAXON Mode 36
 
 sleep 1
 
@@ -73,7 +73,7 @@ lb r0 AUTOMOD Mode 0
 blt r0 6 landing.wait
 
 s button Lock 0
-sb KLAXON Setting 7
+sb KLAXON Mode 7
 
 sleep 10
 
